ce9332585814b2232b435c8cdf4483dae7eacfae
Devdeep Ghosh <devarmforce01@gmail.com>
2023-01-25 17:26:40 +0530

    renderer files added

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const path = require("path")
 const { app, BrowserWindow } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "production";
+const isMac = process.platform === "darwin";
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  // Open devtools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+})
