e8509ff324d536ac3a0c23a81f5cb5504588ebcd
Devdeep Ghosh <devarmforce01@gmail.com>
2023-01-25 18:00:06 +0530

    boilerplate setup and menu created

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const path = require("path")
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
+// Create the main window
 const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 const createMainWindow = () => {
@@ -18,15 +19,57 @@ const createMainWindow = () => {
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+// Create about window
+const createAboutWindow = () => {
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+
+}
+
+// App is ready
 app.whenReady().then(() => {
   createMainWindow();
 
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow();
     }
   });
 });
+
+// Menu template
+const menu = [
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      {
+        label: "About",
+        click: createAboutWindow,
+      }
+    ]
+  }] : []),
+  {
+    role: "fileMenu"
+  },
+  ...(!isMac ? [{
+    label: "Help",
+    submenu: [
+      {
+        label: "About",
+        click: createAboutWindow,
+      }
+    ]
+  }] : [])
+];
 
 app.on("window-all-closed", () => {
   if (!isMac) {
