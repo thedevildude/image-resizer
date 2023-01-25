77e317dde733f02c0d3a32173ecd8915bca6062e
Devdeep Ghosh <devarmforce01@gmail.com>
2023-01-25 16:50:22 +0530

    createMainWindow and index.html

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const path = require("path")
+const { app, BrowserWindow } = require("electron");
+
+const createMainWindow = () => {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
